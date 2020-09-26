1e075d582f27f59c7ef4c172b8eb9161fb0496b3
yl1462 <yliang1462@hotmail.com>
2020-09-26 14:59:51 -0500

    data showing undefined

--- index.js
+++ index.js
@@ -166,7 +166,9 @@ function checkTCQanswer() {
       <h4>Well done!<h4>
       <p>Would you like to try another one?</p>
       <button class='TCQButton' type='submit'>Yes!</button>
-      <button class='closeWindow' type='submit'>That's all for today!</button>
+      <button class='readyGo' type='button'>
+        <span>Back to Main Menu</span>
+      </button>
       `
     )
   }
@@ -192,21 +194,122 @@ function wrongAnswer() {
     <h4>${displayCorrectTCQ()}</h4>
     <p>Would you like to try another one?</p>
     <button class='TCQButton' type='submit'>Yes!</button>
-    <button class='closeWindow' type='submit'>That's all for today!</button>
     `
   )
 }
 
-function closeWindow () {
-  $('.closeWindow').on('click', function(){
-    window.close();
+//here on is for Sentence Equivalence Questions
+
+function SEQButton() {
+  $('main').on('click', '.SEQButton', function () {
+    console.log('displaying SEQ questions');
+    fetchSEQquestions();
+  });
+}
+
+//fetching tc questions
+function fetchSEQquestions() {
+  fetch("https://gre-verbal.p.rapidapi.com/api/v1/questions?subcat=SE&count=10", {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "gre-verbal.p.rapidapi.com",
+      "x-rapidapi-key": "f6bf5906c1msh45ccd6bea4e4595p19e710jsn64d68bc7e7d2"
+    }
+  })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      QuizData = data.data[0];
+      displaySEQquestions(data)
+    })
+}
+
+//display SE questions
+function displaySEQquestions(data) {
+  $('main').html(
+    `
+    <form>
+    <h4>${data.data[0].description}</h4>
+    <p>${SEQcheckBox(data.data[0].options)}</p>
+     <button class='turnIn' type='submit'>Turn in</button>
+    </form>
+     `
+  )
+}
+
+function SEQcheckBox(options) {
+  let option = ''
+  for (let i = 0; i < options.length; i++) {
+    option += `
+    <br>
+    <input type='checkbox'>${data.data[0].options[i]}
+    `
+  }
+  return option
+}
+
+function submitSEQanswer() {
+  $('main').on('submit', 'form', function (event) {
+    event.preventDefault();
+    checkSEQanswer();
   })
 }
+
+function checkSEQanswer() {
+  let incorrect = false
+  for (let i = 0; i < QuizData.answers.length; i++) {
+    let correctAnswer = `${QuizData.answers[0][i]}`
+    let userAnswer = $(`#SEQanswer${i} input:checked`).val();
+    if (correctAnswer !== userAnswer) {
+      incorrect = true
+      break
+    }
+  }
+  if (incorrect) {
+    wrongAnswer()
+  } else {
+    $('main').html(
+      `
+      <h4>Well done!<h4>
+      <p>Would you like to try another one?</p>
+      <button class='SEQButton' type='submit'>Yes!</button>
+      <button class='readyGo' type='button'>
+        <span>Back to Main Menu</span>
+      </button>
+      `
+    )
+  }
+}
+
+function displayCorrectSEQ() {
+  let SEQquestion = `<h2>${QuizData.description}</h2>`
+  let html = ''
+  $('main').html(SEQquestion)
+  for (let i = 0; i < QuizData.answers.length; i++) {
+    html += `
+    <button class='SEQCorrectAnswer' type='submit'>${QuizData.answers[i][0]}</button>
+    `
+  }
+  $('main').html(html)
+}
+
+function wrongAnswer() {
+  console.log("wrong!")
+  $('main').html(
+    `
+    <h3>Not quite there yet... but you are on the right track!</h3>
+    <h4>${displayCorrectSEQ()}</h4>
+    <p>Would you like to try another one?</p>
+    <button class='SEQButton' type='submit'>Yes!</button>
+    `
+  )
+} 
 
 $(
   welcomePage(),
   showQuestionTypes(),
   TCQButton(),
   submitTCQanswer(),
-  closeWindow()
+  SEQButton(),
+  submitSEQanswer()
 )
